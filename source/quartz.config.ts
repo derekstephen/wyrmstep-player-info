6e98db497e50819ebb4f69c154c3a555ee767ece
Derek DeTommaso <detommaso.derek@gmail.com>
2026-04-06 13:17:29 -0400

    vault backup: 2026-04-06 13:17:29

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "The Wyrmstep Chronicles",
+    pageTitleSuffix: "Player Reference Hub",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "wyrmstep.derekstephen.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
