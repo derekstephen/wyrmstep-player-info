e49e875d50394a23c906a460a39f81e611bb0127
Derek DeTommaso <derek@derekstephen.dev>
2026-04-09 00:30:11 -0400

    Update pageTitleSuffix in quartz.config.ts

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "The Wyrmstep Chronicles",
-    pageTitleSuffix: "Player Reference Hub",
+    pageTitleSuffix: "- Wyrmstep Chronicles",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
